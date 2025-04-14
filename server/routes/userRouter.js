15a785ce13c4c1cea07eeb08b52076d01d6352a0
Ronald Kato <ronaldkato.techlead@gmail.com>
2025-04-14 04:34:41 -0400

    Added the user data retrival route.

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { login, register, logout } from "../controllers/userController.js";
+import {
+  login,
+  register,
+  logout,
+  getUser,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/login", login);
 userRouter.post("/register", register);
 userRouter.post("/logout", logout);
+userRouter.get("/get-user-data", getUser);
 
 export default userRouter;
