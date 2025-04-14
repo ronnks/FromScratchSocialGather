1c89c686e06a7f4f1069230d11ae68dbb57246a9
Ronald Kato <ronaldkato.techlead@gmail.com>
2025-04-14 04:32:20 -0400

    Added the user data retrival controller.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -119,3 +119,26 @@ export const logout = async (req, res) => {
       .json({ sucess: false, message: "Server error :: LOGOUT ERROR" });
   }
 };
+
+export const getUser = async (req, res) => {
+  const { userId } = req.body;
+  try {
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ sucess: false, message: "User ID is required" });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ sucess: false, message: "User not found" });
+    }
+
+    res.status(200).json({ sucess: true, user });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Server error :: GET USER ERROR" });
+  }
+};
