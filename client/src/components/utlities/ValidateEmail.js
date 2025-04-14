60cdc5b6319b40928706d527ce8b60944228a8e6
Ronald Kato <ronaldkato.techlead@gmail.com>
2025-04-14 03:37:46 -0400

    Added authentication back to the routes.

--- client/src/components/utlities/ValidateEmail.js
+++ client/src/components/utlities/ValidateEmail.js
@@ -1,7 +1,7 @@
 function validateEmail(input) {
   const emailRegex =
     /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-  if (emailRegex.test(input.value)) {
+  if (emailRegex.test(input)) {
     return true;
   }
   return false;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,17 +5,23 @@ export const register = async (req, res) => {
   const { firstName, lastName, username, email, password } = req.body;
   try {
     if (!firstName || !lastName || !username || !email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+      return res
+        .status(400)
+        .json({ sucess: false, message: "All fields are required" });
     }
 
     const existingUserWithEmail = await User.findOne({ email });
     if (existingUserWithEmail) {
-      return res.status(400).json({ message: "User already exists" });
+      return res
+        .status(400)
+        .json({ sucess: false, message: "User already exists" });
     }
 
     const existingUserWithUsername = await User.findOne({ username });
     if (existingUserWithUsername) {
-      return res.status(400).json({ message: "Username already taken" });
+      return res
+        .status(400)
+        .json({ sucess: false, message: "Username already taken" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -28,47 +34,88 @@ export const register = async (req, res) => {
     });
 
     await newUser.save();
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
+      maxAge: 0.021 * 24 * 60 * 60 * 1000, // 30 minutes
+    });
     console.log("User registered successfully:", newUser);
-    res.status(201).json({ message: "User registered successfully" });
+    res
+      .status(201)
+      .json({ sucess: true, message: "User registered successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error :: REGISTRATION ERROR" });
+    res
+      .status(500)
+      .json({ sucess: false, message: "Server error :: REGISTRATION ERROR" });
     console.log("Registration error:", error);
   }
 };
 
 export const login = async (req, res) => {
-  const { email, password } = req.body;
+  const { input, password } = req.body;
 
   try {
-    if (!email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+    if (!input || !password) {
+      return res
+        .status(400)
+        .json({ sucess: false, message: "All fields are required" });
     }
 
-    const userEmail = await User.findOne({ email });
+    const userEmail = await User.findOne({
+      $or: [{ email: input }, { username: input }],
+    });
     if (!userEmail) {
-      return res.status(400).json({ message: "Invalid email" });
+      return res.status(400).json({ sucess: false, message: "Invalid email" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, userEmail.password);
     if (!isPasswordValid) {
-      return res.status(400).json({ message: "Invalid password" });
+      return res
+        .status(400)
+        .json({ sucess: false, message: "Invalid password" });
     }
 
-    res.status(200).json({ message: "Login successful" });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
+      maxAge: 0.021 * 24 * 60 * 60 * 1000, // 30 minutes
+    });
+
+    res.status(200).json({ sucess: true, message: "Login successful" });
     console.log("User logged in successfully:", userEmail);
   } catch (error) {
     console.error(error);
     console.log("Login error:", error);
-    return res.status(500).json({ message: "Server error :: LOGIN ERROR" });
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Server error :: LOGIN ERROR" });
   }
 };
 
 export const logout = async (req, res) => {
   try {
-    res.status(200).json({ message: "Logout successful" });
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
+    });
+    res.status(200).json({ sucess: true, message: "Logout successful" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Server error :: LOGOUT ERROR" });
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Server error :: LOGOUT ERROR" });
   }
 };
