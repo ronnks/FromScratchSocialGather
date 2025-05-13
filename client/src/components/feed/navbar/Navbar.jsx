0c9822573afb688bbd6c0b31d4534cde226751b8
Ronald Kato <ronaldkato.techlead@gmail.com>
2025-05-13 08:16:12 -0400

    initial desgin implementation of the navbar icons.

--- client/src/components/feed/navbar/Navbar.jsx
+++ client/src/components/feed/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
 import "./NavbarCSS.css";
+import { home_icon } from "./navicons/NavIcons";
 
 function _Navbar() {
   const navigate = useNavigate();
@@ -12,7 +13,9 @@ function _Navbar() {
       <Navbar className="navbarBox">
         <Nav activeKey="/home">
           <Nav.Item>
-            <Nav.Link href="/home">S</Nav.Link>
+            <Nav.Link href="/home">
+              <img className="nav_icon" src={"profile"}></img>
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Form.Control
@@ -23,30 +26,30 @@ function _Navbar() {
         </Nav>
         <Nav activeKey="/home">
           <Nav.Item>
-            <Nav.Link href="/home">Home</Nav.Link>
+            <img className="nav_icon" src={home_icon}></img>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-1">Friends</Nav.Link>
+            <img className="nav_icon" src={home_icon}></img>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-2">videos</Nav.Link>
+            <img className="nav_icon" src={home_icon}></img>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-3">Groups</Nav.Link>
+            <img className="nav_icon" src={home_icon}></img>
           </Nav.Item>
         </Nav>
         <Nav activeKey="/home">
           <Nav.Item>
-            <Nav.Link href="/home">Menu</Nav.Link>
+            <img className="nav_icon" src={home_icon}></img>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-1">socialize</Nav.Link>
+            <img className="nav_icon" src={home_icon}></img>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-2">notifications</Nav.Link>
+            <img className="nav_icon" src={home_icon}></img>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-3">Account</Nav.Link>
+            <img className="nav_icon" src={home_icon}></img>
           </Nav.Item>
         </Nav>
       </Navbar>
---
+++ client/src/components/feed/navbar/navicons/NavIcons.js
@@ -0,0 +1,19 @@
+import friends_icon from "./friends_icon.png";
+import group_icon from "./group_icon.avif";
+import home_icon from "./home_icon.png";
+import menu_icon from "./menu_icon.svg";
+import notification_icon from "./notification_icon.png";
+import socialgather_icon from "./socialgather_icon.webp";
+import socialize_icon from "./socialize_icon.jpg";
+import video_icon from "./video_icon.jpg";
+
+export {
+  friends_icon,
+  group_icon,
+  home_icon,
+  menu_icon,
+  notification_icon,
+  socialgather_icon,
+  socialize_icon,
+  video_icon,
+};
